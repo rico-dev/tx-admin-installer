21836288f13fb3167ecf9681086b4098519a9508
Rico <72346329+rico-dev@users.noreply.github.com>
2021-09-24 19:45:59 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -53,7 +53,7 @@ function txadmin() {
 
   echo -e "${green}This is you Database Data. Please save it, you have 15s"
 
-  expect "If you done press enter"
+  sleep 15
 
   runCommand "cd /home/" "Install TxAdmin please wait..."
 
